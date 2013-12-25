35133819b8ca49f0df967a3b84604e4bf9d322b9
Ander <anderu_ss@hotmail.com>
2013-12-25 13:30:53 +0100

    cambio mañana de navidad ander

---
+++ src/Clases/GrupoConcepto.java
@@ -0,0 +1,26 @@
+package Clases;
+
+public class GrupoConcepto {
+	
+	private int Id;
+	private String grupo;
+	
+	public GrupoConcepto()
+	{
+		Id=0;
+		grupo="Especifique grupo";
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+}
---
+++ src/Clases/TipoConcepto.java
@@ -0,0 +1,26 @@
+package Clases;
+
+public class TipoConcepto {
+	
+	private int Id;
+	private String tipo;
+	
+	public TipoConcepto()
+	{
+		Id=0;
+		tipo="Especifique tipo";
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
